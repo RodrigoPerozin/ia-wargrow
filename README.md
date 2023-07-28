d6dde63e2a322470c0453c976af2630961cd3987
gustavodacostavicente@gmail.com <gustavodacostavicente@gmail.com>
2023-07-28 17:35:02 -0300

    Hello

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# ia-wargrow
-
-# Windows
+# Windows ia-wargrow
 - py -3.8 -m venv venv
 - cd venv/Scripts
 - ./activate
@@ -8,7 +6,7 @@
 - cd..
 - pip install -r requirements.txt
 - python main.py
+- http://127.0.0.1:8000/docs
 
-
-
-http://127.0.0.1:8000/docs
+Refs:
+https://www.python.org/downloads/release/python-380/
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+import uvicorn 
 
 app = FastAPI()
 
@@ -7,6 +8,5 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
