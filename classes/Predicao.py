6053ddefbaa714e1428f2cc51745294b11196a31
RodrigoPerozin <rodrigodperozin@gmail.com>
2023-08-21 19:19:40 -0300

    feat: new attributes on Predicao class

--- classes/Predicao.py
+++ classes/Predicao.py
@@ -6,11 +6,15 @@ class Predicao:
     height = 0
     confidence = 0
     class_name = ""
+    image_path = ""
+    predicao_type = ""
     
-    def __init__(self, x, y, largura, altura, confianca, classe):
+    def __init__(self, x, y, largura, altura, confianca, classe, caminhoImagem, predicao_tipo):
         self.x = x
         self.y = y
         self.width = largura
         self.height = altura
         self.confidence = confianca
         self.class_name = classe
+        self.image_path = caminhoImagem
+        self.predicao_type = predicao_tipo
