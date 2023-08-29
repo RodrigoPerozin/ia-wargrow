1ae792c75e391962c41c74f21137e3950c546199
dariokrugerjunior <krugerdario1@gmail.com>
2023-08-29 19:51:51 -0300

    removendo async

--- Util/save_temp_image.py
+++ Util/save_temp_image.py
@@ -1,7 +1,7 @@
 import os
 
 
-async def save_temp_image(pil_image):
+def save_temp_image(pil_image):
     temp_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "temp")
     if not os.path.exists(temp_folder):
         os.makedirs(temp_folder)
