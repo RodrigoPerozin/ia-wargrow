b9d46a04d637173b7b1b7842b7840eed34e67d55
RodrigoPerozin <rodrigodperozin@gmail.com>
2023-09-11 20:13:50 -0300

    fix: remove unused functions and elements

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 import os
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\projetos\ia-war\ia-wargrow\key.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
     
 if __name__ == "__main__":
-    uvicorn.run("Controller.ImagePredictionController:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("Controller.ImagePredictionController:app", host="localhost", port=8000, reload=True)
