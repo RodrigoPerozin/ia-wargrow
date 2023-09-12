89548a0f6069e79af8dc840a743f200bf4e182af
RodrigoPerozin <rodrigodperozin@gmail.com>
2023-09-11 21:56:40 -0300

    fix: comment unused function

--- war-node/src/routes/warPredictionRoutes.js
+++ war-node/src/routes/warPredictionRoutes.js
@@ -8,6 +8,65 @@ const attackController = require("../controller/attackController");
 app.use(express.json());
 app.use(cors());
 
+
+app.post("/attack", (req, res) => {
+    
+    const data = req.body;
+    
+    const colorTeam = req.query.color;
+    const objId = req.query.obj;
+    
+    if (!data || !data.length || !colorTeam) {
+        return res.status(400).json({ message: "Dados de ataque inválidos." });
+    }
+
+    const bestMove = attackController.doAttack(data, colorTeam);
+    const objResult = {};
+    let msg = "";
+
+    if (bestMove) {
+        msg += `${bestMove.attacker} ataca ${bestMove.defender}`;
+    } else {
+        msg = "Não ataque!"
+    }
+
+    objResult.message = msg
+    return res.status(200).json(objResult);
+
+});
+
+app.post("/move-troop", (req, res) => {
+    const data = req.body;
+    const colorTeam = req.query.color;
+    
+    if (!data || !data.length || !colorTeam) {
+        return res
+            .status(400)
+            .json({ message: "Dados de movimentação de tropas inválidos." });
+    }
+
+    const handledCountries =
+        warPredictionController.findBestTransfersToReinforce(data, colorTeam);
+
+    moveTroopArray = [];
+    
+    handledCountries.forEach((country) => {
+        if (!moveTroopArray.includes(country)) {
+            moveTroopArray.push(country);
+        }
+    });
+
+    const moveTroopMessages = moveTroopArray.map((country) => {
+        return country;
+    });
+    const responseObj = {
+        message: moveTroopMessages.join("\n"),
+    };
+
+    return res.status(200).json(responseObj);
+});
+
+/*
 app.post("/movement", (req, res) => {
     const data = req.body;
     const troops = req.query.troops;
@@ -51,64 +110,7 @@ app.post("/movement", (req, res) => {
 
     return res.status(200).json(responseObj);
 });
-
-app.post("/attack", (req, res) => {
-
-    const data = req.body;
-
-    const colorTeam = req.query.color;
-    const objId = req.query.obj;
-
-    if (!data || !data.length || !colorTeam) {
-        return res.status(400).json({ message: "Dados de ataque inválidos." });
-    }
-
-    const bestMove = attackController.doAttack(data, colorTeam);
-    const objResult = {};
-    let msg = "";
-
-    if (bestMove) {
-        msg += `${bestMove.attacker} ataca ${bestMove.defender}`;
-    } else {
-        msg = "Não ataque!"
-    }
-
-    objResult.message = msg
-    return res.status(200).json(objResult);
-
-});
-
-app.post("/move-troop", (req, res) => {
-    const data = req.body;
-    const colorTeam = req.query.color;
-
-    if (!data || !data.length || !colorTeam) {
-        return res
-            .status(400)
-            .json({ message: "Dados de movimentação de tropas inválidos." });
-    }
-
-    const handledCountries =
-        warPredictionController.findBestTransfersToReinforce(data, colorTeam);
-
-    moveTroopArray = [];
-
-    handledCountries.forEach((country) => {
-        if (!moveTroopArray.includes(country)) {
-            moveTroopArray.push(country);
-        }
-    });
-
-    const moveTroopMessages = moveTroopArray.map((country) => {
-        return country;
-    });
-    const responseObj = {
-        message: moveTroopMessages.join("\n"),
-    };
-
-    return res.status(200).json(responseObj);
-});
-
+*/
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
