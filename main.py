bebd29703d9eca976f87d89f99768b0badb13ff8
RodrigoPerozin <rodrigodperozin@gmail.com>
2023-09-11 21:43:52 -0300

    fix: troop-moviment validation

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import uvicorn
 import os
 
-<<<<<<< Updated upstream
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "key.json"
-=======
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/nicolas/studyspace/projects-senai/ia-wargrow/key.json"
->>>>>>> Stashed changes
     
 if __name__ == "__main__":
     uvicorn.run("Controller.ImagePredictionController:app", host="localhost", port=8000, reload=True)
