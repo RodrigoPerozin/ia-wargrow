5cbe6be9c4ccd71ff6a38207b03351aff3b0958a
dariokrugerjunior <krugerdario1@gmail.com>
2023-09-13 19:26:48 -0300

    Correções no robôflow

--- Model/RoboflowPredictor.py
+++ Model/RoboflowPredictor.py
@@ -5,8 +5,8 @@ class RoboflowPredictor:
         self.rf = Roboflow(api_key="81gG6yXrCsPPzbHBRCuK")
         self.project = self.rf.workspace().project("war-ia")
         self.model = self.project.version(1).model
-        self.confidence = 20
-        self.overlap = 20
+        self.confidence = 7
+        self.overlap = 1
 
     async def predict_json(self, image_path):
         return self.model.predict(image_path, confidence=self.confidence, overlap=self.overlap).json()
